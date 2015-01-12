045fbc010e75c2c834c5943c9e1a4adfd0cfb202
Angela Bier <angelabier@Angelas-MacBook-Pro.local>
2015-01-11 23:23:34 -0500

    Comment out all tests except for setup in order to locate errors.

--- code/blackjack_test.rb
+++ code/blackjack_test.rb
@@ -16,28 +16,35 @@ class TestBlackjack < MiniTest::Unit::TestCase
     #if a player's hand is less than 21, the player is given the option to hit or stay
       #if a player stays, the dealer moves to the next player.
       #if a player hits, the dealer deals one random card to the player.
-        #if hand total equals 21, the dealer moves to next player
-        #if hand total is greater than 21, the player busts
-        #if hand total is less than 21, the player is given the option to hit or stay.(loop)
-    #if a player's hand equals 21, the dealer moves to the next player and cards are analyzed.
+        #if new hand total equals 21, the dealer moves to next player
+        #if new hand total is greater than 21, the player busts
+        #if new hand total is less than 21, the player is given the option to hit or stay.(loop)
+    #if a player's hand equals 21, player has Blackjack and the dealer moves to the next player.
   #the game ends when
+
+  #nouns: deck, dealer, player, hand,
+  #verbs: deal, hit, stay
 
   def setup #this runs before each test
     @blackjack = Blackjack.new
   end
 
-  def test_blackjack_exists
-    assert Blackjack.new
-  end
+  #def test_blackjack_exists
+  #  assert @blackjack
+  #end
 
-  def test_deck_exists
-    assert Blackjack.new.deck
-  end
+  #def test_deck_exists
+  #  assert @blackjack.deck
+  #end
+
+ # def test_deck_has_52_cards
+ #   assert_equal 52, cards.length
+ # end
 
 =begin
   def test_blackjack_deck_has_cards
-    assert Game.new.grid.is_a? Array
-    assert_equal Game.new.grid.length, 3
+
+
   end
 =end
 end
