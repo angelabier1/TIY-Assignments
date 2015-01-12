08b43b177dd243278af83955a5603db33bf54ed5
Angela Bier <angelabier@Angelas-MacBook-Pro.local>
2015-01-12 05:51:59 -0500

    Add Dealer class. Initialize with empty hand and hand_total = 0. Add attr_accessor for :hand and :hand_total. Also add @dealer to Blackjack initialize method. Need to add attr_accessor for :dealer.

--- code/blackjack.rb
+++ code/blackjack.rb
@@ -1,13 +1,14 @@
 class Blackjack
-  attr_accessor :deck #this means we can interact with the instance variable @deck
+  attr_accessor :deck, :hand #this means we can interact with the instance variable @deck
   # and we can pretend it is a method: Blackjack.new.deck
   def initialize #this runs when Blackjack.new is called
     @deck = Deck.new
+    @dealer = Dealer.new
   end
 
- # def hand
- #   @hand = hand
- # end
+ #def hand
+  #  @hand = []
+  #end
 end
 
 class Deck
@@ -28,14 +29,17 @@ class Deck
     @shuffled_cards = @cards.shuffle
   end
 
- # def deal #remove one card from shuffled deck
+ # def deal #remove one card from shuffled deck - possible to do yield twice to get two initial cards?
  #   @cards.pop
  # end
 end
 
-#class Dealer
-  #attr_accessor
- # def initialize
+class Dealer
+  attr_accessor :hand, :hand_total
+  def initialize
+    @hand = []
+    @hand_total = 0
+  end
 
- # end
+  end
 #end
