cb65715252a545005b84ccec94ac9915bbbd6caa
Angela Bier <angelabier@Angelas-MacBook-Pro.local>
2015-01-12 14:51:41 -0500

    Require minitest/autorun and ./wolf.

---
+++ code/wolf_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+#require 'minitest/pride'
+require './wolf'
+
+#Wolf has a name and age
+#Wolf can join a pack
+#Wolf can howl
+#wolf can how louder if older than 3 than if it's younger
+
+class TestWolf < MiniTest::Unit::TestCase
+
+end
