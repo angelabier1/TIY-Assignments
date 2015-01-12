c6a1d1d419cd23f5a43aa53a3de7a19551225a4f
Angela Bier <angelabier@Angelas-MacBook-Pro.local>
2015-01-12 18:08:54 -0500

    Add join_pack, howl, howl_louder, submissive_to and dominant_to methods. Test all methods.

--- code/wolf.rb
+++ code/wolf.rb
@@ -1,9 +1,47 @@
+#Wolf has a name and age
+#Wolf can join a pack
+#Wolf can howl
+#Wolf can how louder if older than 3 than if it's younger
+
 class Wolf
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :wolf
 
   def initialize(name,age)
     @name = name
     @age = age
   end
 
+  def join_pack
+    true
+  end
+
+  def howl
+    "howwwlllll!"
+  end
+
+  def howl_louder
+    if self.age > 3
+      "HOWWWWWWLLLLLL!"
+    else
+      "#{@name} is too young to howl any louder!"
+    end
+  end
+
+
+  def submissive_to(wolf)
+      if self.age < wolf.age
+        true
+      else
+        false
+      end
+  end
+
+
+  def dominant_to(wolf)
+    if self.age > wolf.age
+        true
+      else
+        false
+    end
+  end
   end
