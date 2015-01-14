c6e3dde19ada015140edbc308a03ec1a88be1a5a
Angela Bier <angelabier@Angelas-MacBook-Pro.local>
2015-01-14 13:52:45 -0500

    update blackjack_test.rb

--- code/blackjack_test.rb
+++ code/blackjack_test.rb
@@ -11,9 +11,12 @@ class TestBlackjack < MiniTest::Unit::TestCase
     # A has a value of 11
       #Card values: 2,3,4,5,6,7,8,9,10,10,10,10,11 (13 Cards) * 4 Suits = 52 Cards
   #there is one dealer
+    #dealer starts with empty hand
+    #empty hand equals 0
   #there are 1+ player(s)
-  #the dealer deals a hand to each player and self.
-    #a hand is 2 random cards from the deck
+    #each player receives 2 cards from shuffled deck.
+      # for each player "deal shuffled_cards.pop times 2"
+        #hand total = 0, deal 2 cards, new hand total = sum of two dealt cards?
     #if a player's hand is less than 21, the player is given the option to hit or stay
       #if a player stays, the dealer moves to the next player.
       #if a player hits, the dealer deals one random card to the player.
@@ -21,10 +24,19 @@ class TestBlackjack < MiniTest::Unit::TestCase
         #if new hand total is greater than 21, the player busts
         #if new hand total is less than 21, the player is given the option to hit or stay.(loop)
     #if a player's hand equals 21, player has Blackjack and the dealer moves to the next player.
-  #the game ends when
+  #Dealer and each Player, play against each other -- compare Dealer's total to Player's total.
+  #the game ends when there are no cards left in deck?
+
+  #class Card
+  #has a suit and a value
+  #displaying a card should be a string, like "2 of Hearts" or "King of Spades"
+  #in blackjack, teh value of jack/queen/king are all 10 and ace is 11
+
 
   #nouns: deck, dealer, player, hand,
-  #verbs: deal, hit, stay
+  #verbs: deal, hit, stay, shuffle, bust
+
+
 
   def setup #this runs before each test
     @blackjack = Blackjack.new
@@ -36,6 +48,14 @@ class TestBlackjack < MiniTest::Unit::TestCase
 
   def test_deck_exists
     assert @blackjack.deck
+  end
+
+  #def test_deal_method_returns_integer_from_deck
+   # assert
+  #end
+
+  def test_hand_has_two_cards
+    assert_equal 2, @blackjack.hand.length
   end
 
 end
@@ -54,6 +74,16 @@ class TestDeck < MiniTest::Unit::TestCase
     assert_equal false, @deck.cards == @deck.shuffled_cards
   end
 
+end
+
+class TestPlayer < Minitest::Unit::TestCase
+  def setup
+    @player = Blackjack.new.player
+  end
+
+  #def test_hand_dealt_has_2_cards
+   # assert_equal 2, @player.hand.length
+  #end
 end
 
 class TestDealer < MiniTest::Unit::TestCase
