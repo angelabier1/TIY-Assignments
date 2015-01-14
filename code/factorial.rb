9020be5447f8c3f97a1aab3ca5c5511b4160a421
Angela Bier <angelabier@Angelas-MacBook-Pro.local>
2015-01-14 13:55:50 -0500

    add test to factorial.rb

--- code/factorial.rb
+++ code/factorial.rb
@@ -1,5 +1,8 @@
-def factorial(num)  
-  (1..num).inject(1) {|a, b| a * b} # the "1" initial value allows it to work for 0  
-end  
+class Fixnum
+	def factorial(num)  
+    (1..num).inject(1) {|a, b| a * b} # the "1" initial value allows it to work for 0  
+	end  
+end
 
 puts factorial(20)
+puts factorial(45)
