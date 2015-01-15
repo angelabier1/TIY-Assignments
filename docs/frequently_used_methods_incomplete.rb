eab53b2175ae4966f606c5bdcf44d9e6d36d39d0
Angela Bier <abier@corelando.com>
2015-01-15 06:40:40 -0500

    create separate file for frequently used methods that do not have examples yet

---
+++ docs/frequently_used_methods_incomplete.rb
@@ -0,0 +1,71 @@
+Enumerable
+all?
+any?
+detect
+each_with_index
+grep
+group_by
+inject
+max
+max_by
+member?
+min
+min_by
+partition
+reduce
+
+String
+*
++
+<<
+=~
+		[]
+[]=
+		capitalize(!)
+chomp(!)
+downcase(!)
+each_line
+empty?
+gsub(!)
+include?
+sub(!)
+to_i
+to_f
+to_sym
+
+
+Object
+class
+inspect
+	instance_of?
+	is_a?
+	kind_of?
+	methods
+	nil?
+	respond_to?
+	send
+	tap
+
+	Hash
+	[]
+	[]=delete
+	each
+	each_key
+	each_value
+	empty?
+	fetch
+	has_key?
+	has_value?
+	include?
+	invert
+	keep_if
+	key
+	keys
+	length
+	merge(!)
+	reject(!)
+	select(!)
+	values
+
+	Range
+	nested_array =
