37e9a1b59cb2941341851298c2332fa2998bc821
Angela Bier <angelabier@Angelas-MacBook-Pro.local>
2015-01-14 22:25:39 -0500

    clean up repo. delete files

--- code/blackjack.rb
+++
@@ -1,7 +0,0 @@
-class Blackjack
-  attr_accessor :deck #this means we can interact with the instance variable @deck
-  # and we can pretend it is a method: Blackjack.new.deck
-  def initialize #this runs when Blackjack.new is called
-    @deck = {{ }}
-  end
-end
