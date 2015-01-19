585ad8a07e3d97a4d1d58e435fc58b46e7bd5137
Angela Bier <abier@corelando.com>
2015-01-18 21:31:04 -0500

    test values for :jack, :queen, :king and :ace

--- code/three_card_poker/card_test.rb
+++ code/three_card_poker/card_test.rb
@@ -15,4 +15,28 @@ class CardTest < MiniTest::Unit::TestCase
 		assert_equal 5, @card.rank
 	end
 
+	def test_card_has_value
+		assert_equal 5, @card.value
+	end
+
+	def test_jack_valued_at_11
+		card = Card.new(:jack, :diamonds)
+		assert_equal 11, card.value
+	end
+
+	def test_queen_valued_at_12
+		card = Card.new(:queen, :hearts)
+		assert_equal 12, card.value
+	end
+
+	def test_king_valued_at_13
+		card = Card.new(:king, :hearts)
+		assert_equal 13, card.value
+	end
+
+	def test_ace_valued_at_14
+		card = Card.new(:ace, :hearts)
+		assert_equal 14, card.value
+	end
+
 end
