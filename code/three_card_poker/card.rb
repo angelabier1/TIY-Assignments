9d2a302bca03a70edaf09d8cf341a0d197920a66
Angela Bier <abier@corelando.com>
2015-01-18 21:31:32 -0500

    add value method

--- code/three_card_poker/card.rb
+++ code/three_card_poker/card.rb
@@ -7,4 +7,18 @@ class Card
 		@suit = suit
 	end
 
+	def value
+		if @rank.is_a?(Fixnum)
+			@rank
+		elsif @rank == :jack
+			11
+		elsif @rank == :queen
+			12
+		elsif @rank == :king
+			13
+		else
+			14
+		end
+	end
+
 end
