86530e248b44ecb223f472fe0ef6fe0ecaad78fc
Angela Bier <abier@corelando.com>
2015-01-20 14:08:54 -0500

    update three_card_poker.rb and three_card_poker_test.rb

--- code/three_card_poker/three_card_poker.rb
+++ code/three_card_poker/three_card_poker.rb
@@ -1,18 +1,22 @@
 require './player'
 require './dealer'
+require './card'
+require './deck'
+require './hand'
 
 class ThreeCardPoker
 
-	attr_reader :player, :dealer, :winner, :push
+	attr_reader :player, :dealer, :winner, :push, :deck, :cards
 	attr_accessor :pair_plus, :ante, :hand
 
 	def initialize(deck = nil)
 
 		@deck = deck || Deck.new
-		@cards = @deck.deal(3)
-		@hand = Hand.new(@cards)
-		@player = Player.new(@hand, @pair_plus=0,@ante = 0)
-		@dealer = Dealer.new(@hand)
+		player_cards = @deck.deal(3)
+		player_hand = Hand.new(player_cards)
+		@player = Player.new(player_hand, @pair_plus=0,@ante = 0)
+		dealer_hand = Hand.new(@deck.deal(3))
+		@dealer = Dealer.new(dealer_hand)
 		check_for_winner
 	end
 
@@ -22,25 +26,26 @@ class ThreeCardPoker
 	end
 
 	def check_for_winner
+		@winner =
 		if @player.hand.value  > @dealer.hand.value
-			@winner = @player
+			 @player
 		elsif @player.hand.value < @dealer.hand.value
-			@winner = @dealer
+			@dealer
 		elsif @player.hand.value == @dealer.hand.value && @player.hand.type != :pair
 			if @player.hand.high_value > @dealer.hand.high_value
-				@winner = @player
+				 @player
 			elsif @player.hand.high_value < @dealer.hand.high_value
-				@winner = @dealer
+				 @dealer
 			else
 				if @player.hand.mid_value > @dealer.hand.mid_value
-					@winner = @player
+					 @player
 				elsif @player.hand.mid_value < @dealer.hand.mid_value
-					@winner = @dealer
+					 @dealer
 				else
 					if @player.hand.low_value > @dealer.hand.low_value
-						@winner = @player
+						@player
 					elsif @player.hand.low_value < @dealer.hand.low_value
-						@winner = @dealer
+						@dealer
 					else
 						@push = true
 					end
@@ -48,15 +53,15 @@ class ThreeCardPoker
 			end
 		else
 			if @player.hand.pair_rank[0] > @dealer.hand.pair_rank[0]
-				@winner = @player
+				 @player
 			elsif
 				@player.hand.pair_rank[0] < @dealer.hand.pair_rank[0]
-				@winner = @dealer
+					@dealer
 			else
 				if @player.hand.kicker[0] > @dealer.hand.kicker[0]
-					@winner = @player
+					@player
 				elsif @player.hand.kicker[0] < @dealer.hand.kicker[0]
-					@winner = @dealer
+					@dealer
 				else
 					@push = true
 				end
--- code/three_card_poker/three_card_poker_test.rb
+++ code/three_card_poker/three_card_poker_test.rb
@@ -9,7 +9,15 @@ require './dealer_test'
 class ThreeCardPokerTest < MiniTest::Unit::TestCase
 
 	def setup
-		@game = ThreeCardPoker.new
+		@deck = Deck.new
+		@game = ThreeCardPoker.new(@deck)
+		player_cards = @deck.deal(3)
+		player_hand = Hand.new(player_cards)
+		@player = Player.new(player_hand)
+		dealer_cards = @deck.deal(3)
+		dealer_hand = Hand.new(dealer_cards)
+		@dealer = Dealer.new(dealer_hand)
+
 	end
 
 	def test_game_has_a_player
@@ -29,8 +37,17 @@ class ThreeCardPokerTest < MiniTest::Unit::TestCase
 	end
 
 	def test_player_dealt_straight_wins_if_dealer_has_pair
-		deck = Deck.new([Card.new(10,:spades),Card.new(10,:diamonds),Card.new(2,:diamonds),Card.new(8,:clubs),Card.new(9,:hearts),Card.new(10,:spades)])
+		cards = ([Card.new(10,:spades),Card.new(10,:diamonds),Card.new(2,:diamonds),Card.new(8,:clubs),Card.new(9,:hearts),Card.new(10,:spades)])
+		deck = Deck.new(cards)
 		game = ThreeCardPoker.new(deck)
-		game.check_for_winner
+		puts "Winner is #{@game.winner} "
+	end
+
+	def test_show_player_hand_and_player_hand_type
+		puts "Player has:\n#{@player.hand.card_display}\nWhich is a #{@player.hand.type_display} hand."
+	end
+
+	def test_show_dealer_hand_and_dealer_hand_type
+		puts "Dealer has:\n#{@dealer.hand.card_display}\nWhich is a #{@dealer.hand.type_display} hand."
 	end
 end
