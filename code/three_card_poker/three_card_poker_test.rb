cb1d7a4d0b1dff3d69118be52b888aa925be94ed
Angela Bier <abier@corelando.com>
2015-01-20 02:41:48 -0500

    add test game has a player and dealer. test player and dealer start with 3 cards. test player dealt straight wins if dealer has pair

--- code/three_card_poker/three_card_poker_test.rb
+++ code/three_card_poker/three_card_poker_test.rb
@@ -1,7 +1,36 @@
 require 'minitest/autorun'
-require 'three_card_poker'
+require './three_card_poker'
+require './card_test'
+require './player_test'
+require './deck_test'
+require './hand_test'
+require './dealer_test'
 
-class ThreeCardPokerTest > MiniTest::Unit::TestCase
+class ThreeCardPokerTest < MiniTest::Unit::TestCase
 
 	def setup
+		@game = ThreeCardPoker.new
+	end
+
+	def test_game_has_a_player
+		assert @game.player
+	end
+
+	def test_game_has_a_dealer
+		assert @game.dealer
+	end
+
+  def test_player_starts_with_3_cards_from_deck
+		assert_equal 3, @game.player.hand.size
+  end
+
+	def test_dealer_starts_with_3_cards_from_deck
+		assert_equal 3, @game.dealer.hand.size
+	end
+
+	def test_player_dealt_straight_wins_if_dealer_has_pair
+		deck = Deck.new([Card.new(10,:spades),Card.new(10,:diamonds),Card.new(2,:diamonds),Card.new(8,:clubs),Card.new(9,:hearts),Card.new(10,:spades)])
+		game = ThreeCardPoker.new(deck)
+		game.check_for_winner
+	end
 end
