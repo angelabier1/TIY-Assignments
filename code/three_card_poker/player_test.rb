d6ec18d20815c6fc873123466925618bc8534d39
Angela Bier <abier@corelando.com>
2015-01-20 02:43:06 -0500

    test player has hand and player hand has type

--- code/three_card_poker/player_test.rb
+++ code/three_card_poker/player_test.rb
@@ -1,11 +1,14 @@
 require 'minitest/autorun'
 require './player'
+require './hand'
+require './card'
 
 class PlayerTest < MiniTest::Unit::TestCase
 
 	def setup
 		cards = [Card.new(:ace,:clubs), Card.new(5,:clubs), Card.new(10, :spades)]
-		@player = Player.new(cards)
+		@hand = Hand.new(cards)
+		@player = Player.new(@hand, @pair_plus = 0, @ante = 0)
 	end
 
 	def test_player_has_hand
@@ -14,7 +17,7 @@ class PlayerTest < MiniTest::Unit::TestCase
 
 	def test_player_hand_has_type
 		#cards = [Card.new(:ace,:clubs), Card.new(5,:clubs), Card.new(10, :clubs)]
-		assert_equal :flush, @player.hand_type
+		assert_equal :high_card, @player.hand_type
 	end
 
 
