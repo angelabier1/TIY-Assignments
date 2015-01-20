7ae530d839228ac17dca143e5fa64399209ceedd
Angela Bier <abier@corelando.com>
2015-01-19 21:04:17 -0500

    remove deck.rb as requirement. add card.rb as required file. add test_hand_has_cards_with_different_suits

--- code/three_card_poker/hand_test.rb
+++ code/three_card_poker/hand_test.rb
@@ -1,11 +1,10 @@
 require 'minitest/autorun'
 require './hand'
-require './deck'
+require './card'
 
 class HandTest < MiniTest::Unit::TestCase
 
 	def setup
-		deck = Deck.new
 		cards = [Card.new(2, :hearts),Card.new(4,:diamonds),Card.new(6,:clubs)]
 		@hand = Hand.new(cards)
 	end
@@ -17,6 +16,12 @@ class HandTest < MiniTest::Unit::TestCase
 
 	def test_hand_has_cards_with_different_ranks
 		assert_equal 3, @hand.unique_ranks
+	end
+
+	def test_hand_has_cards_with_different_suits
+		cards = [Card.new(2, :hearts),Card.new(4,:hearts),Card.new(6,:clubs)]
+		hand2 = Hand.new(cards)
+		assert_equal 1, hand2.unique_suits
 	end
 
 
