e115d035a218c0b185b646e0915742400def030a
Angela Bier <abier@corelando.com>
2015-01-20 22:40:08 -0500

    test_jack_valued_at_11

--- code/cards/three_card_poker/poker_card_test.rb
+++ code/cards/three_card_poker/poker_card_test.rb
@@ -2,8 +2,15 @@ require 'minitest/autorun'
 require './poker_card'
 
 class PokerCardTest < MiniTest::Unit::TestCase
+
 	def setup
-		@poker_card =
+		@poker_card = Card.new(:ace,:hearts)
 	end
+
+	def test_jack_valued_at_11
+		assert_equal 11, @poker_card.value
+	end
+
+
 
 end
