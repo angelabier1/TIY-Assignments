e2392b2baa7e0d64b36e4a407fa35ee441aaee06
Angela Bier <abier@corelando.com>
2015-01-21 16:31:55 -0500

    add card.rb and deck.rb to use with erb files

---
+++ code/three_card_poker/card.rb
@@ -0,0 +1,37 @@
+require './to_english'
+class Card
+
+	attr_reader :suit, :rank
+
+	def initialize(rank, suit)
+		@rank = rank
+		@suit = suit
+	end
+
+	def value
+		if @rank.is_a?(Fixnum)
+			@rank
+		elsif @rank == :jack
+			11
+		elsif @rank == :queen
+			12
+		elsif @rank == :king
+			13
+		else
+			14
+		end
+	end
+
+	def display
+		if @rank.is_a?(Fixnum)
+			"#{@rank.to_english.capitalize} of #{suit.to_s.capitalize}"
+		else
+			"#{@rank.to_s.capitalize} of #{suit.to_s.capitalize}"
+		end
+	end
+
+	def to_img
+		"<img src="
+	end
+
+end
---
+++ code/three_card_poker/deck.rb
@@ -0,0 +1,27 @@
+require './card'
+class Deck
+
+	def initialize (cards = nil)
+		@cards = cards || [:hearts, :diamonds, :clubs, :spades].map do |suit|
+				((2..10).to_a + [:jack, :queen, :king, :ace]).map do |rank|
+				Card.new(rank,suit)
+				end
+		end.flatten.shuffle!
+	end
+
+	def size
+		@cards.length
+	end
+
+	def [](index)
+		@cards[index]
+	end
+
+	def deal(n)
+		@cards.pop(n)
+	end
+
+
+
+
+end
